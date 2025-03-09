ec220f79267bf22daaa2e002b6668b000f82f8c5
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-09 13:13:18 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+TOKEN = "7760154469:AAFMteXwMHZU27J0aCqlUjqgaheZaJf6T9w"
+BOT_USERNAME = "ShadowRewardsBot"
+# You can mix numeric IDs and usernames (usernames without '@')
+OWNERS = ["7218606355", "MrLazyOp"]
+ADMINS = ["7218606355", "MrLazyOp"]
+REQUIRED_CHANNELS = [
+    "https://t.me/shadowsquad0",
+    "https://t.me/Originlabs",
+    "https://t.me/ShadowsquadHits",
+    "https://t.me/Binhub_Originlabs"
+]
