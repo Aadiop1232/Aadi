f8324e706c3f7a0fff3104ed4a9cab0b8b2faf2e
Aadiop1232 <khanaadi2030@gmail.com>
2025-03-09 17:39:26 +0530

    Update account_info.py

--- handlers/account_info.py
+++ handlers/account_info.py
@@ -3,30 +3,15 @@ from db import get_user, add_user
 from datetime import datetime
 
 def send_account_info(bot, update):
-    """
-    Sends the account information for the user who triggered the update.
-    This version displays the sender's Telegram user ID (from_user.id) as the User ID.
-    Works with both Message and CallbackQuery objects.
-    """
-    # Retrieve the sender's Telegram ID from update.from_user
     telegram_id = str(update.from_user.id)
-    
-    # Determine chat_id: if update has message, use its chat id; otherwise, use telegram_id.
-    chat_id = update.message.chat.id if hasattr(update, "message") and update.message else telegram_id
+    chat_id = update.message.chat.id if hasattr(update, "message") else telegram_id
 
-    # Retrieve user info from the database; if not registered, add the user on the fly.
     user = get_user(telegram_id)
     if not user:
-        add_user(
-            telegram_id,
-            update.from_user.username or update.from_user.first_name,
-            datetime.now().strftime("%Y-%m-%d")
-        )
+        add_user(telegram_id, update.from_user.username or update.from_user.first_name, datetime.now().strftime("%Y-%m-%d"))
         user = get_user(telegram_id)
     
-    # Database schema for users is assumed to be:
-    # (telegram_id, internal_id, username, join_date, points, referrals, banned, pending_referrer)
-    # We now display only the Telegram ID (user[0]) as the "User ID".
+    # Dynamically generate text based on user data
     text = (
         f"<b>👤 Account Info 😁</b>\n"
         f"• <b>Username:</b> {user[2]}\n"
